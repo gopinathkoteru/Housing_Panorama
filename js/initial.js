7a181f521f373e64ca784d6d3527ed75af62194d
Gopinath <gopinathkoteru1180@gmail.com>
2015-06-09 19:19:16 +0530

    removed global variables and made Hotspot an object

--- js/initial.js
+++ js/initial.js
@@ -1,51 +1,62 @@
 var camera, scene, renderer;
-var raycaster = new THREE.Raycaster();;
-
-var isUserInteracting = false;
-var onMouseDownMouseX = 0, onMouseDownMouseY = 0;
-var onMouseDownLon = 0;
-var onMouseDownLat = 0;
-var phi = 0, theta = 0;
+var raycaster = new THREE.Raycaster();
 
 var images = {};
 
-var img_name = ['r','l','u','d','f','b'];
-var mouse_speed = 0.3;
+var Config = 
+{
+    imgName :['r','l','u','d','f','b'],
+    mouseSpeed : 0.3,
+    isUserInteracting :false,
+    phi : 0, 
+    theta : 0,
+    lon :0,
+    lat :0,
+    keyMax : 7,
+    keySpeed:2
+};
 
-
-
-var pano = function( panoid, is_blur ){
+var Pano = function( panoid, is_blur ){
     this.panoid = panoid;
     this.name = "panorama";
     this.isBlur = is_blur;
 };
 
+var Hotspot = {
+    panoid : 0,
+    hotspotAngles : []
+};
+
 var Transition = 
 {
     path : "",
-    hotspotAngles : [],
     currentPano : 0,
     panoNum : 0,
     moving : false
 };
 
-Transition.init = function( path, hotspots_angle )
+Transition.init = function( path)
 {
     Transition.path = path;
-    Transition.hotspotAngles = hotspots_angle;
     
     var path = path + "1/" + "mobile_";
-    Transition.blurPano = new pano(1,true);
-    var clearPano1 = new pano(1,false);
-    var clearPano2 = new pano(1,false);
+    Transition.blurPano = new Pano(1,true);
+    var clearPano1 = new Pano(1,false);
+    var clearPano2 = new Pano(1,false);
     
     Transition.blurPano.createPano(path,0.0);
     clearPano1.createPano(path,1.0);
     clearPano2.createPano(path,0.0);
 
     Transition.clearPano = [clearPano1,clearPano2];
+
     Transition.preloadImages();  
 };
+
+Hotspot.init = function(hotspots_angle)
+{
+    Hotspot.hotspotAngles = hotspots_angle;
+}
 
 init();
 animate();
@@ -58,10 +69,6 @@ function init()
 
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1100 );
     camera.target = new THREE.Vector3( 0, 0, 0 );
-
-    camera.lon = 0;
-    camera.lat = 0;
-
 
     scene = new THREE.Scene();
 
@@ -103,14 +110,14 @@ function animate()
 
 function update()
 {
-    camera.lon = (camera.lon + 360)%360;
-    camera.lat = Math.max( - 35, Math.min( 35,camera.lat ) );
-    phi = THREE.Math.degToRad( 90 - camera.lat );
-    theta = THREE.Math.degToRad( camera.lon );
+    Config.lon = (Config.lon + 360)%360;
+    Config.lat = Math.max( - 35, Math.min( 35,Config.lat ) );
+    Config.phi = THREE.Math.degToRad( 90 - Config.lat );
+    Config.theta = THREE.Math.degToRad( Config.lon );
 
-    camera.target.x = 500 * Math.sin( phi ) * Math.cos( theta );
-    camera.target.y = 500 * Math.cos( phi );
-    camera.target.z = 500 * Math.sin( phi ) * Math.sin( theta );
+    camera.target.x = 500 * Math.sin( Config.phi ) * Math.cos( Config.theta );
+    camera.target.y = 500 * Math.cos( Config.phi );
+    camera.target.z = 500 * Math.sin( Config.phi ) * Math.sin( Config.theta );
 
     camera.lookAt( camera.target );
 
